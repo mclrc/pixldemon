447bfddd190a8a38017ad0c52fcefeeb819ac122
pixldemon <moritzamando@gmail.com>
2021-12-18 16:41:27 +0100

    Update stack & content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,27 +3,18 @@
     <div class="slide-in-contents page-content" id="home-content-wrapper">
       <h1>Hello There!</h1>
       <p>
-        My name is Moritz. I’m a german tech enthusiast in my teens.
-        Since I fell in love with computers and programming a few years ago, I have tried to learn all that I can
-        about a variety of topics related to computer science and software engineering.
+        My name is Moritz. I’m a german tech enthusiast in my teens. Since I
+        fell in love with computers and programming a few years ago, I have
+        tried to learn all that I can about a variety of topics related to
+        computer science and software engineering.
       </p>
       <p>
-        The goal of this site is to catalog my development journey, but also to motivate myself to create more cool stuff to put on here.
-      </p>
-      <!-- <p>
-        I do some web development on the side, so if you like what you see and need a website done,
-        or are interested in potentially collaborating, just shoot me an email or a DM! No commitments. I’m always happy to get to know new people.
-      </p> -->
-      <div class="container">
-        <PrimaryButton label="Take a Look at My Work!" @click="$router.push('portfolio')" />
-      </div>
-      <p>
-        Right now, I'm working on the front end of
-        <a href="https://votyvote.com/">VotyVote</a>, an awesome online polling platform.
+        The goal of this site is to catalog my development journey, but also to
+        motivate myself to create more cool stuff to put on here.
       </p>
       <h2>My Arsenal</h2>
       <div id="stack-wrapper">
-        <div id="stack">
+        <div id="items">
           <div class="stack-item" v-for="item in items" :key="item.title">
             <img :src="item.icon" :alt="item.title" />
             <div>
@@ -33,15 +24,25 @@
           </div>
         </div>
       </div>
+      <div class="container">
+        <PrimaryButton
+          label="Take a Look at My Work!"
+          @click="$router.push('portfolio')"
+        />
+      </div>
       <p>
-        Those are my preferred tools for pretty much anything webdev.
-        With the exception of data storage, I've gone through a bunch of different solutions each, and eventually settled on this stack.
+        I've also written my own front-end library,
+        <a href="https://github.com/pixldemon/yaduil">yaduil</a>, but that's
+        more of an experiment that an attempt to create something useful in
+        practice. Writing basic tooling myself is something I love. Not because
+        it's productive, but because it's fun and teaches you a lot.
       </p>
       <p>
-        I've also begun developing my own front-end framework,
-        <a href="https://github.com/pixldemon/visiejs">visiejs</a>, but that's more of an experiment that an attempt
-        to create something useful in practice. I'm pretty much completely rewriting it, which is why the repo is private,
-        but it should be ready to go public again soon.
+        In the past, I've done mainly web and gamedev, but am currently dabbling
+        in physics simulations with
+        <a href="https://github.com/pixldemon/traj-propagate">traj-propagate</a
+        >, a trajectory propagation tool for astronomical objects and
+        spacecraft.
       </p>
     </div>
   </div>
@@ -55,34 +56,86 @@ export default {
   name: "Home",
   components: {
     TypingElement,
-    PrimaryButton
+    PrimaryButton,
   },
   data() {
     return {
       items: [
         {
+          icon: "/assets/tsicon.jpg",
+          title: "Typescript",
+          description: "For less headaches with anything JS",
+        },
+        {
           icon: "/assets/vue.png",
           title: "Vue.js",
-          description: "For lightning fast, dynamic UI"
+          description: "For lightning fast, dynamic UI",
         },
         {
-          icon: "/assets/flask.png",
-          title: "Flask",
-          description: "For an infinitely extendable back end"
+          icon: "/assets/rusticon.png",
+          title: "Rust",
+          description: "For performance, convenience and safety",
         },
         {
-          icon: "/assets/sqlite.png",
-          title: "SQLite",
-          description: "For safe & fast storage of data"
-        }
-      ]
+          icon: "/assets/pythonicon.png",
+          title: "Python",
+          description: "For quick data processing and scripting",
+        },
+        {
+          icon: "/assets/archicon.png",
+          title: "Arch Linux",
+          description: "For freedom and productivity",
+        },
+        {
+          icon: "/assets/cliicon.png",
+          title: "The Command Line",
+          description: "For getting things done",
+        },
+      ],
     };
-  }
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../styles/_variables.scss";
+#items {
+  display: grid;
+  grid-gap: 14px;
+  width: 100%;
+  justify-items: space-between;
+  grid-template-columns: repeat(auto-fill, minmax(250px, auto));
+  .stack-item {
+    min-height: 200px;
+    transition: 0.2s;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    :not(:last-child) {
+      margin-right: 0.4rem;
+    }
+    img {
+      height: 120px;
+      margin-bottom: 1rem;
+    }
+    p,
+    h3 {
+      margin: 0.3rem 0;
+      text-align: center;
+    }
+    p {
+      font-size: 1.2rem;
+      line-height: 1.4 !important;
+      color: $text-color;
+      text-align: center;
+    }
+    &:not(:last-child) {
+      margin-right: 1rem;
+    }
+  }
+}
 
 #home-wrapper {
   width: 100%;
@@ -92,63 +145,5 @@ export default {
 hr {
   margin: 1rem 0;
   border: none;
-}
-
-#stack-wrapper {
-  margin: 2rem auto;
-  height: 100%;
-  display: flex;
-  align-items: center;
-}
-#stack {
-  display: flex;
-  margin: 0 auto;
-}
-.stack-item {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  img {
-    height: 120px;
-    margin-bottom: 1rem;
-  }
-  p,
-  h3 {
-    margin: 0.3rem 0;
-    text-align: center;
-  }
-  p {
-    font-size: 1.2rem;
-    line-height: 1.4 !important;
-    color: $text-color;
-    text-align: center;
-  }
-  &:not(:last-child) {
-    margin-right: 1rem;
-  }
-}
-
-@media only screen and (max-width: $breakpoint) {
-  .stack-item {
-    flex-direction: row;
-    margin-right: 0 !important;
-    p,
-    h4 {
-      font-size: 1.3rem;
-      text-align: left;
-    }
-    img {
-      width: 100px;
-      height: auto;
-      padding: 2rem;
-      margin: 0;
-      box-sizing: content-box;
-    }
-  }
-  #stack {
-    display: block;
-    margin: 0;
-  }
 }
 </style>
